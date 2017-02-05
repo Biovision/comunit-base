717311bc12968165ef3da15d07bef190a88b278b
Maxim Khan-Magomedov <maxim.km@gmail.com>
2017-02-05 20:10:50 +0300

    Маршруты и контроллер профилей

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,80 @@
 Rails.application.routes.draw do
+  resources :users, except: [:show]
+  resources :tokens, :codes, except: [:index, :show]
+  resources :privileges, except: [:index, :show]
+  resources :regions, except: [:index, :show]
+
+  namespace :admin do
+    resources :users, only: [:index, :show] do
+      member do
+        get 'tokens'
+        get 'codes'
+        get 'privileges'
+      end
+    end
+    resources :tokens, :codes, only: [:index, :show]
+
+    resources :privileges, only: [:index, :show] do
+      get 'users', on: :member
+    end
+    resources :regions, only: [:index, :show] do
+      get 'cities', on: :member
+    end
+  end
+
+  namespace :api, defaults: { format: :json } do
+    resources :tokens, except: [:new, :edit] do
+      post 'toggle', on: :member
+    end
+    resources :users, except: [:new, :edit] do
+      member do
+        put 'follow'
+        delete 'follow' => :unfollow
+        put 'privileges/:privilege_id' => :grant_privilege, as: :privilege
+        delete 'privileges/:privilege_id' => :revoke_privilege
+        post 'toggle'
+      end
+    end
+    resources :privileges, except: [:new, :edit] do
+      member do
+        put 'lock'
+        delete 'lock', action: :unlock
+        post 'priority'
+        post 'toggle'
+      end
+    end
+    resources :regions, except: [:new, :edit] do
+      member do
+        post 'toggle'
+      end
+    end
+  end
+
+  namespace :network, defaults: { format: :json } do
+    controller :sites do
+      put 'sites/:id' => :synchronize, as: :synchronize_site
+    end
+    scope 'users', controller: :users do
+      put ':id' => :synchronize, as: :synchronize_user
+    end
+  end
+
+  namespace :my do
+    get '/' => 'index#index'
+
+    resource :profile, except: [:destroy]
+    resource :confirmation, :recovery, only: [:show, :create, :update]
+  end
+
+  scope 'u/:slug', controller: :profiles do
+    get '/' => :show, as: :user_profile
+    get 'entries' => :entries, as: :user_profile_entries
+    get 'followees', as: :user_profile_followees
+  end
+
+  controller :authentication do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
 end
