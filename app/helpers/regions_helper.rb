5897ee76e135586e9bdf33eeaecabf42afbf3ae2
Maxim Khan-Magomedov <maxim.km@gmail.com>
2017-06-29 14:44:42 +0300

    Выбор регионов по иерархии

--- app/helpers/regions_helper.rb
+++ app/helpers/regions_helper.rb
@@ -20,9 +20,27 @@ module RegionsHelper
     options
   end
 
-  def current_region_for_select(selected_id)
+  # @param [Integer] selected_id
+  def current_region_for_select(selected_id = 0)
     options = [['Центр', '', { data: { url: root_path } }]]
     Region.visible.for_tree.each do |region|
+      url = regional_index_path(region_slug: region.long_slug)
+      options << [region.name, region.id, { data: { url: url } }]
+    end
+
+    options_for_select(options, selected_id)
+  end
+
+  # @param [Integer] region_id
+  # @param [Integer] selected_id
+  def child_regions_for_select(region_id, selected_id = 0, slug = '')
+    options = []
+    if region_id.to_i < 1
+      options << ['Центр', '', { data: { url: root_path } }]
+    else
+      options << ['Все', '', { data: { url: regional_index_path(region_slug: slug) } }]
+    end
+    Region.visible.for_tree(region_id).each do |region|
       url = regional_index_path(region_slug: region.long_slug)
       options << [region.name, region.id, { data: { url: url } }]
     end
