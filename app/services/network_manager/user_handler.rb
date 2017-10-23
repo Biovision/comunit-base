7ab0e7748d1ab90936764e163091dda6d982fbca
Maxim Khan-Magomedov <maxim.km@gmail.com>
2017-10-23 16:05:03 +0300

    Отправка данных о пользователе в центр

--- app/services/network_manager/user_handler.rb
+++ app/services/network_manager/user_handler.rb
@@ -1,5 +1,9 @@
 class NetworkManager::UserHandler < NetworkManager
 
+  # Обновить данные о пользователе извне
+  #
+  # Вызывается центральным сайтом при создании или обновлении пользователя
+  #
   # @param [User] entity
   # @param [Hash] attributes
   # @param [Hash] data
@@ -19,12 +23,51 @@ class NetworkManager::UserHandler < NetworkManager
     entity.user_profile&.update!(profile_attributes)
   end
 
+  # Синхронизировать нового пользователя
+  #
+  # Отправляет пользователя в центральный сайт и привязывает его внешний id
+  # и site_id
+  #
   # @param [User] user
   def relink_user(user)
     log_event("Relinking user #{user.id}")
     url     = "#{MAIN_HOST}/network/users/relink"
+    data    = prepare_user_data(user)
+    unless user.agent.nil?
+      data[:data][:agent_name] = user.agent.name
+    end
+    log_event("Data: #{data.inspect}\n")
+
+    response = RestClient.post(url, JSON.generate(data), request_headers)
+    log_event("Response (#{response.code}):\n#{response.body.inspect}\n")
+
+    if user.external_id.blank?
+      parsed   = JSON.parse(response).dig('data')
+      new_data = {
+        external_id: parsed['user_id'],
+        site_id:     parsed['site_id']
+      }
+      user.update!(new_data)
+    end
+  end
+
+  # @param [User] user
+  def sync_user(user)
+    log_event("Syncing user #{user.id}")
+    url  = "#{MAIN_HOST}/network/users/#{user.external_id}"
+    data = prepare_user_data(user)
+    log_event("Data: #{data.inspect}\n")
+
+    response = RestClient.put(url, JSON.generate(data), request_headers)
+    log_event("Response (#{response.code}):\n#{response.body.inspect}\n")
+  end
+
+  private
+
+  # @param [User] user
+  def prepare_user_data(user)
     allowed = User.relink_parameters
-    # Залепа для переходного периода
+
     attributes = user.attributes.select { |a| allowed.include?(a) }
     unless user.user_profile.nil?
       allowed_in_profile = UserProfile.entity_parameters
@@ -35,22 +78,10 @@ class NetworkManager::UserHandler < NetworkManager
       user: attributes,
       data: Hash.new
     }
-    unless user.agent.nil?
-      data[:data][:agent_name] = user.agent.name
-    end
     unless user.image.blank?
       data[:data][:image_path] = user.image.url
     end
-    log_event("Data: #{data.inspect}\n")
-    response = RestClient.post(url, JSON.generate(data), request_headers)
-    log_event("Response (#{response.code}):\n#{response.body.inspect}\n")
-    if user.external_id.blank?
-      parsed   = JSON.parse(response).dig('data')
-      new_data = {
-        external_id: parsed['user_id'],
-        site_id:     parsed['site_id']
-      }
-      user.update!(new_data)
-    end
+
+    data
   end
 end
