c74ef3e8a60f769f385e7cd9089acecbbcbf0696
Maxim Khan-Magomedov <maxim.km@gmail.com>
2019-04-14 19:27:47 +0300

    No link to post illustration from post

--- app/models/post.rb
+++ app/models/post.rb
@@ -41,7 +41,7 @@ class Post < ApplicationRecord
   has_many :post_links, dependent: :delete_all
   has_many :post_post_tags, dependent: :destroy
   has_many :post_tags, through: :post_post_tags
-  has_many :post_illustrations, dependent: :delete_all
+  # has_many :post_illustrations, dependent: :delete_all
   has_many :post_images, dependent: :destroy
   has_many :post_translations, dependent: :delete_all
   has_many :post_zen_categories, dependent: :destroy
