7aea87fafbbf78c4ed1cd332243e00e051722a37
Maxim Khan-Magomedov <maxim.km@gmail.com>
2019-10-25 23:20:18 +0300

    Using pg_search in SearchController

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,10 @@
+# frozen_string_literal: true
+
+# Searching for posts
 class SearchController < ApplicationController
   # get /search?q=
   def index
-    @query = param_from_request(:q).to_s[0..100]
-    if @query.blank?
-      @collection = []
-    else
-      @collection = Elasticsearch::Model.search(@query, Post).records.first(20)
-      # @collection = Post.pg_search(@query).list_for_visitors.first(50)
-    end
+    q = param_from_request(:q).to_s[0..100]
+    @collection = q.blank? ? [] : Post.pg_search(q).list_for_visitors.first(50)
   end
 end
--- lib/comunit/base/version.rb
+++ lib/comunit/base/version.rb
@@ -2,6 +2,6 @@
 
 module Comunit
   module Base
-    VERSION = '0.28.191023.0'
+    VERSION = '0.28.191025.0'
   end
 end
